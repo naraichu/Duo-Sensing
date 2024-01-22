f5b3db6ed36b3e7696eeaeea1f752489c1a3a172
Chai Worajitwannakul <naraichu@gmail.com>
2024-01-22 16:11:41 +0000

    Change in serial reading

--- serial_read.py
+++ serial_read.py
@@ -1,97 +1,42 @@
-import serial as s
+import serial
+import numpy as np
 
-# Replace 'COM3' with the actual serial port your Arduino is connected to
-serial_port = 'COM12'
 
-# Replace 9600 with the baud rate your Arduino is using
-baud_rate = 9600
+# Specify the COM port and baud rate
+com_port = 'COM12'    # Change this to the appropriate COM port
+baud_rate = 115200    # Change this to the baud rate used by your Arduino
+
+# Arrays for graph plotting
+freq_array = np.zeros(200)
+value_array = np.zeros(200)
+
+# Flag boolean
+isFreq = False
+
 
 # Open the serial port
-ser = s.Serial(serial_port, baud_rate)
+ser = serial.Serial(com_port, baud_rate, timeout = 1)
 
 try:
     while True:
-        # Read a line from the serial port and decode it
-        line = ser.readline()
+        # Read a line of data from the serial port as bytes
+        data_bytes = ser.read()
 
-        # Print the received data
-        print(line)
+
+        try:
+            data_value = int.from_bytes(data_bytes, byteorder = 'little', signed = False)
+            
+            #if (isFreq):
+                #freq_array
+            print(data_value)
+            
+       
+
+        except ValueError:
+            # Handle the case where the received data is not a valid integer
+            print("Invalid data received:", x_value)
 
 except KeyboardInterrupt:
-    # Close the serial port when the script is interrupted (Ctrl+C)
+    # Close the serial port on KeyboardInterrupt (Ctrl+C)
     ser.close()
-
-
-
-
-
-
-'''
-# Credit from https://www.youtube.com/watch?v=PFPwcolQho0&t=743s
-
-from typing import List, Tuple
-import serial as s
-
-ARRAY_SIZE = 160
-
-def get_from_serial(port: s.Serial) -> Tuple[List, List]:
-    x_axis = []
-    y_axis = []
-    flag = 0
-    n = 0
-    
-    while True:
-        dat = port.read(1).hex()
-        if dat == "00":
-            n += 1
-            ar = []
-            for j in range(7):
-                ar.append(port.read(1).hex())
-            e = int(ar[6], base = 16)
-
-            # Check for command word
-            cmd = int(ar[0], base = 16)
-
-            if (cmd == 2):
-                print("Start of array")
-                x_axis = []
-                y_axis = []
-                flag = 1
-                continue
-            
-            elif (cmd == 3):
-                print("End of array")
-                if flag == 1:
-                    return (x_axis, y_axis)
-
-            # Run checksum by adding all the bytes except the checksum
-            check_sum = sum([int(d, base =16) for d in ar[0:6]]) % 255
-
-            # If the checksums do not match then exit
-            if check_sum != e:
-                print(">> Checksum Error: ", check_sum)
-                flag = 0
-                continue
-
-            z = int(ar[5], base = 16)
-
-            # Parsing the zero byte and updating zeroes
-            if (z & 1 != 0):
-                ar[2] = "00"
-            if (z & 2 != 0):
-                ar[1] = "00"
-            if (z & 4 != 0):
-                ar[4] = "00"
-            if (z & 8 != 0):
-                ar[3] = "00"
-
-            y = int(ar[1] + ar[2], base = 16)
-            x = int(ar[3] + ar[4], base = 16)
-            x_axis.append(x)
-            y_axis.append(y)
-
-
-if __name__ == "__main__":
-    port = s.Serial("COM12", 115200)
-    print(get_from_serial(port))
-'''
+    print("Serial port closed.")
