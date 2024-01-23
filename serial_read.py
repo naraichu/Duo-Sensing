1ec2c398cb074b435921b3bdb5722cf577107382
Chai Worajitwannakul <naraichu@gmail.com>
2024-01-23 17:17:30 +0000

    PY serial works on continous serial stream

--- serial_read.py
+++ serial_read.py
@@ -1,17 +1,12 @@
 import serial
-import time
 
-ser = serial.Serial('COM12', 115200, timeout=1)
+ser = serial.Serial('COM12', 115200)  # Replace 'COMx' with the actual port your Arduino is connected to
 
-def send_command(command):
-    ser.write(command.encode('utf-8'))
-    time.sleep(0.1)  # Allow time for the Arduino to process the command
-
-def read_integer():
+def read_float():
     while True:
         if ser.readable():
             try:
-                data = ser.readline().decode(errors='replace').strip()
+                data = ser.readline().decode().strip()
                 if data.startswith('F') or data.startswith('V'):
                     return int(data[1:])
             except UnicodeDecodeError as e:
@@ -20,19 +15,12 @@ def read_integer():
                 ser.reset_input_buffer()
 
 try:
-    # Send "START" command to Arduino to initiate data stream
-    send_command("START")
-    print("START sent")
-
-    # Read data from Arduino
     while True:
-        integer_value = read_integer()
-        if integer_value is not None:
-            print("Received integer value:", integer_value)
-            # Do something with the integer value
+        read_value = read_float()
+        if read_value is not None:
+            print("Value:", read_value)
+            # Do something with the float value
 
 except KeyboardInterrupt:
-    # Send "STOP" command to Arduino to stop data stream
-    send_command("STOP")
     ser.close()
     print("Serial connection closed.")
