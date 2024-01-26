70b8d6fa8477e579f1c8e3d7ff12312f62476701
Chai Worajitwannakul <naraichu@gmail.com>
2024-01-26 15:09:56 +0000

    Stream of data (unstable sometimes)

--- serial_read.py
+++ serial_read.py
@@ -1,14 +1,40 @@
 import serial
+import time
 
-ser = serial.Serial('COM12', 115200)  # Replace 'COMx' with the actual port your Arduino is connected to
 
-def read_float():
+ser = serial.Serial('COM12', 115200, timeout = 1)  # Replace 'COMx' with the actual port your Arduino is connected to
+
+
+try:
+    time.sleep(1)
+    while True:
+        # Check if there is data available in the input buffer
+        if ser.in_waiting > 0:
+            # Read the data from the serial port
+            data = ser.readline().decode(encoding = "utf-8").strip()
+            
+            # Process or print the received data
+            print("Received data:", data)
+        
+        # Add a small delay to avoid high CPU usage
+        time.sleep(0.005)
+
+except KeyboardInterrupt:
+    # Close the serial port on program exit
+    ser.close()
+    print("Serial port closed.")
+
+
+
+'''
+def read_serial():
     while True:
         if ser.readable():
             try:
-                data = ser.readline().decode().strip()
-                if data.startswith('F') or data.startswith('V'):
-                    return int(data[1:])
+                time.sleep(0.001)
+                data = ser.readline().decode(encoding= "utf-8").strip()
+                return data
+
             except UnicodeDecodeError as e:
                 print(f"Error decoding data: {e}")
                 # Add additional handling if needed, such as clearing the serial buffer
@@ -16,11 +42,23 @@ def read_float():
 
 try:
     while True:
-        read_value = read_float()
+        read_value = read_serial()
         if read_value is not None:
-            print("Value:", read_value)
+            print(read_value)
+            #print("Value:", read_value)
             # Do something with the float value
 
 except KeyboardInterrupt:
     ser.close()
     print("Serial connection closed.")
+'''
+
+'''
+ser = serial.Serial("COM12", 115200)
+
+while True:
+    ser.write(1.encode())
+    time.sleep(1)
+    ser.write("END".encode())
+    time.sleep(1)
+'''
