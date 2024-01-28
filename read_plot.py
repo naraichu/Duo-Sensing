47baacde234525962b7e212ad224c7f6024b6abd
Chai Worajitwannakul <naraichu@gmail.com>
2024-01-28 12:53:27 +0000

    Complete version of reading serial and ploting data

--- read_plot.py
+++ read_plot.py
@@ -1,20 +1,31 @@
 import serial
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 
 # Input number of frequency being swept
-array_len = 200
+freq_len = 200
 
 # Number of byte length to be decoded
 # offset is 2 because for 0 and -100
-byte_len = (array_len + 2) * 2
-
+byte_len = (freq_len + 2) * 2
 
 # Declare port number and baudrate
 ser = serial.Serial('COM12', 115200)
 
 # Generate frequency array (+1 because of 0)
-x_axis = np.arange(array_len + 1)
+x_axis = np.arange(freq_len + 1)
+
+# Generate zero value array first
+y_axis = np.zeros((freq_len + 1), dtype = int)
+
+
+# Initialise matplotlib
+fig, ax = plt.subplots()
+line, = ax.plot([], [], 'o', markersize = 1)  # Initialize an empty line
+ax.set(xlim=[0, freq_len], ylim=[0, 800], xlabel='Frequency', ylabel='Amplitude')
+
 
 
 def read_serial_data():
@@ -28,12 +39,13 @@ def read_serial_data():
             
             # Check if the data is correct
             if is_data_valid(y_axis):
-                y_axis = np.delete(y_axis, -1)  # Remove -100 (last value) from array
-                
-                plot_array = np.stack((x_axis, y_axis), axis = -1) # Stack x and y into 2D array
-                
-                print(plot_array)
-                
+                y_axis = np.delete(y_axis, -1)  # Remove -100 (last value) from y_axis
+
+                return y_axis
+
+                # Output result
+                #print(y_axis)
+                #print("\n")
 
             else:
                 # Close and reopen the serial connection
@@ -48,12 +60,23 @@ def read_serial_data():
 
 
 def is_data_valid(y_axis):
-    if array_len + 2 == len(y_axis) and y_axis[array_len + 1] == -100:
+    if freq_len + 2 == len(y_axis) and y_axis[freq_len + 1] == -100:
         return True
     else:
         return False
 
 
+def update(frame):
+    line.set_ydata(read_serial_data())
+    return line,
+
 
 if __name__ == "__main__":
-    read_serial_data()
+    line, = ax.plot(x_axis, read_serial_data())
+    ani = FuncAnimation(fig, update, frames = None, interval = 200, cache_frame_data = False)
+
+    # Show the plot
+    plt.xlabel('Frequency')
+    plt.ylabel('Amplitude')
+    plt.title('SFCS')
+    plt.show()
