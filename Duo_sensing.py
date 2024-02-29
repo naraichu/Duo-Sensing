dc33988fbcc245fd8b797e2f5ed720409de5843a
Chai Worajitwannakul <naraichu@gmail.com>
2024-02-29 11:23:18 +0000

    Add backup data when invalid

--- Duo_sensing.py
+++ Duo_sensing.py
@@ -1,9 +1,14 @@
 import serial
 import numpy as np
 import time
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
 
 # NOTE
 # This is the duo-sensing program to read value from both resistive and capacitive sensing.
+
+
 
 # Input number of frequency being swept
 freq_len = 200
@@ -18,39 +23,81 @@ ser = serial.Serial('COM12', 115200)
 # Flag when Arduino is ready
 isStart = False
 
-# Using previous array of data when actual data invalid
-backup_array = np.zeros(freq_len + 3)
+# Arrays for Capacitive sensing (SFCS)
+cap_x_axis = np.arange(freq_len + 1, dtype = int)
+
+
+# Backup value in case there is error in data transmission
+res_back_value = 0
+cap_y_back_axis = np.zeros(freq_len + 2, dtype = int)
 
 
 def read_serial():
-    global backup_array  # declare backup_array as global
-
+    global cap_y_back_axis, res_back_value
     try:
         while True:
+            # Read data from the serial port
             data = ser.read(byte_len)
-
-            all_array = np.array([int.from_bytes(data[i:i + 2], byteorder='little', signed=True) for i in
-                                  range(0, byte_len, 2)])
-
-            # Check if the data is correct
+            
+            # Convert the received bytes back to signed integers array
+            all_array = np.array([int.from_bytes(data[i:i + 2], byteorder = 'little', signed = True) for i in range(0, byte_len, 2)])
+            
+            # If valid then seperate data and return
             if is_data_valid(all_array):
-                all_array = np.delete(all_array, -2)  # Remove -100 (last value) from all_array
-                backup_array = all_array
-                print(all_array)
+                
+                # Seperate data out into each catergory
+                res_value, cap_y_axis = SeperateData(all_array)
+                
+                # Store latest value as the backup
+                global cap_y_back_axis, res_back_value
+                cap_y_back_axis = cap_y_axis
+                res_back_value = res_value
 
+                # Return reading values
+                # return res_value, cap_y_axis
+
+                
+                # Display Duo-sensing values
+                print("SFCS: ", cap_y_axis)
+                print("Res:", res_value)
+                print("\n")
+                
+
+            # If not valid, return last known data
             else:
-                # Close and reopen the serial connection
-                #print(">> Invalid data")
-                print(backup_array)
-
+                # Restart the serial connection
                 ser.close()
                 ser.open()
 
+                # Return backup values
+                #return res_back_value, cap_y_back_axis
+                
+                
+                # Putput error message
+                print(">> Invalid, display last known values...")
+                
+                # Display last known value of the data
+                print("SFCS: ", cap_y_back_axis)
+                print("Res:", res_back_value)
+                print("\n")
+                
+
     except KeyboardInterrupt:
+        # Close the serial connection when the program is interrupted
         print("<-- Port closed -->")
         ser.close()
 
 
+# Seperate data out from the array into appropriate sensing types
+def SeperateData(all_array):
+    all_array = np.delete(all_array, -2)    # Pop -100 (last 2 value) out
+    res_value = all_array[-1]               # Get the resistive value in last index
+    cap_y_axis = np.delete(all_array, -1)   # Pop res_value out
+
+    return res_value, cap_y_axis
+
+    
+# Check whether the value is valid
 def is_data_valid(all_array):
     if freq_len + 3 == len(all_array) and all_array[freq_len + 1] == -100:
         return True
@@ -60,9 +107,8 @@ def is_data_valid(all_array):
 
 # Main function
 if __name__ == "__main__":
-    if isStart:
-        read_serial()
-    else:
+
+    if not isStart:
         print(">> Waiting 6 seconds for Arduino to setup...")
         time.sleep(6)
         isStart = True
