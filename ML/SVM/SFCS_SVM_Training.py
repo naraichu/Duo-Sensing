f6a587a9c1134252c02ff0a00e3a89cd11f23c52
Chai Worajitwannakul <naraichu@gmail.com>
2024-03-08 12:43:07 +0000

    Save model seperately

--- ML/SVM/SFCS_SVM_Training.py
+++ ML/SVM/SFCS_SVM_Training.py
@@ -4,11 +4,17 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+import pickle
 
-file_path = "C:/Users/acer/OneDrive - University of Bath/Subjects/Year 3/CM30082 Individual Project/Arduino_Software/Duo_Tactile_Software/ML/datasets.json"
+
+# Files path for training data and saving the model
+json_path = "C:/Users/acer/OneDrive - University of Bath/Subjects/Year 3/CM30082 Individual Project/Arduino_Software/Duo_Tactile_Software/ML/datasets.json"
+pickle_path = "C:/Users/acer/OneDrive - University of Bath/Subjects/Year 3/CM30082 Individual Project/Arduino_Software/Duo_Tactile_Software/ML/SVM/SFCS_SVM.pkl"
+
+
 
 # Load dataset from JSON
-with open(file_path, 'r') as f:
+with open(json_path, 'r') as f:
     dataset = json.load(f)
 
 # Extract features and labels from the dataset
@@ -26,6 +32,10 @@ X_test = sc.transform(X_test)
 # Train SVM with RBF kernel
 svm_classifier = SVC(kernel='rbf', random_state=42)
 svm_classifier.fit(X_train, y_train)
+
+# Save the trained model using pickle
+with open(pickle_path, 'wb') as f:
+    pickle.dump(svm_classifier, f)
 
 # Predictions
 y_pred = svm_classifier.predict(X_test)
